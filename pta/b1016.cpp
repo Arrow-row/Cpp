098be1880e8798b1167f92804b774c3ad5150b91
Arrow <1512803831@qq.com>
2022-02-24 11:53:56 +0800

    pta

--- pta/b1016.cpp
+++ pta/b1016.cpp
@@ -4,11 +4,13 @@ int main(){
 	long a,b,da,db,pa=0,pb=0;
 	scanf("%ld %ld %ld %ld",&a,&da,&b,&db);
 	while(a!=0){
-		if(a%10==da) pa=pa*10+da;
+		if(a%10==da) 	//依次求a的各位上的数
+			pa=pa*10+da;
 		a/=10;
 	}
 	while(b!=0){
-		if(b%10==db) pb=pb*10+db;
+		if(b%10==db) 
+			pb=pb*10+db;
 		b/=10;
 	}
 	printf("%d",pa+pb);
